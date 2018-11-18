167d65ccbf5cae5d69eb2f667618c7a2e8d9e9f8
R Heckers <robertoheckers@gmail.com>
2018-11-18 21:06:10 +0100

    authAction api request

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -2,14 +2,11 @@ import { GET_ERRORS } from './types';
 import axios from 'axios';
 
 //Register User
-export const registeruser = userData => dispatch => {
+export const registeruser = (userData, history) => dispatch => {
     axios.post('/api/users/register', userData)
-      .then(res => {
-        console.log(res.data)
-      })
-      .catch(err => 
-        dispatch({
-            type: GET_ERRORS,
-            payload: err.response.data
-        }));
+      .then(res => history.push('/login'))
+      .catch(err => dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      }));
 };
